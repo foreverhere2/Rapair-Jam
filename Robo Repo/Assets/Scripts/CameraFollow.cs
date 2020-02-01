27d44db845643cc3a61eaa3c4bc37641254ccede
Mint16384 <mint16384@gmail.com>
2020-02-01 01:03:35 -0500

    camera slightly increases in size during jump

--- Robo Repo/Assets/Scripts/CameraFollow.cs
+++ Robo Repo/Assets/Scripts/CameraFollow.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
+    public Camera cam;
+    public Movement player;
     
     private Vector3 empty = Vector3.zero;
 
-    void Update() { transform.position = Vector3.SmoothDamp(transform.position, player.TransformPoint(Vector3.back * 10f), ref empty, .3f); }
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.gameObject.transform.TransformPoint(Vector3.back * 10f), ref empty, .3f);
+        if(!player.isGrounded && cam.orthographicSize < 6f)
+        {
+            cam.orthographicSize *= 1.0005f;
+        }
+        else if(cam.orthographicSize > 5f)
+        {
+            cam.orthographicSize /= 1.001f;
+        }
+    }
 }
