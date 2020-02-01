b79135c1b97e6e0cf033b3426df0ebfe2e0e99a8
Jamear <vjamear@gmail.com>
2020-01-31 22:26:18 -0500

    Create movement.cs

---
+++ Robo Repo/Assets/Scripts/movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+
+    public float speed;
+    public float jump;
+    float moveVelocity;
+
+
+    bool isGrounded = true;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (isGrounded)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+                isGrounded = false;
+            }
+        }
+
+        moveVelocity = 0;
+
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isGrounded = true;
+    }
+}
