2792b93f3f34836abff626e9faea96c467920ee0
Mint16384 <mint16384@gmail.com>
2020-02-02 04:04:15 -0500

    enemy sprite turns toward player

--- Robo Repo/Assets/Scripts/EnemyFollow.cs
+++ Robo Repo/Assets/Scripts/EnemyFollow.cs
@@ -5,24 +5,28 @@ using UnityEngine;
 public class EnemyFollow : MonoBehaviour
 {
     public float speed;
-    public static float health = 3;
-
+    public bool isDead = false;
     public Animator animator;
     public Transform target;
+    public Rigidbody2D rigidBody;
+    public SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         target = target == null ? GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>() : target;
         animator = animator == null ? gameObject.GetComponent<Animator>() : animator;
+        rigidBody = rigidBody == null ? gameObject.GetComponent<Rigidbody2D>() : rigidBody;
+        spriteRenderer = spriteRenderer == null ? gameObject.GetComponent<SpriteRenderer>() : spriteRenderer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) < 2)
+        if (Vector2.Distance(transform.position, target.position) < 8 && !isDead)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, Vector2.right * target.position.x + Vector2.up * transform.position.y, speed * Time.deltaTime);
         }
+        spriteRenderer.flipX = (target.position.x - transform.position.x) > 0 ? false : (target.position.x - transform.position.x) < 0 ? true : spriteRenderer.flipX;
     }
 }
