be148d62b1c3177b0bf6a01ee54385e1616d0ffc
Mephistophilian <43144386+Mephistophilian@users.noreply.github.com>
2020-02-02 15:38:21 -0500

    stuff

---
+++ Robo Repo/Assets/Scripts/Breakable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+
+    public Animator thisAnim;
+    public PolygonCollider2D thisCollider;
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("PlayerPunch"))
+        {
+            thisAnim.SetBool("isDestroyed", true);
+            thisCollider.enabled = false;
+        }
+    }
+}
