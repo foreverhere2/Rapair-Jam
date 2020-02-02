c84627a72f33709d36e254017cec871859f98245
Mephistophilian <43144386+Mephistophilian@users.noreply.github.com>
2020-02-02 16:34:45 -0500

    vafe

--- Robo Repo/Assets/Scripts/Movement.cs
+++ Robo Repo/Assets/Scripts/Movement.cs
@@ -124,7 +124,7 @@ public class Movement : MonoBehaviour
     {
         thisAnim.SetTrigger("LegsAquisition");
         isDead = true;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         isDead = false;
         thisAnim.SetTrigger("FinishAnim");
     }
