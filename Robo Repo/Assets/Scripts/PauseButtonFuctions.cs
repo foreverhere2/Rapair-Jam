bf5762c23202e04f5eada6ef8bec22a4a7ba388c
Mint16384 <mint16384@gmail.com>
2020-02-02 16:21:55 -0500

    added text to beginning of gameplay

--- Robo Repo/Assets/Scripts/PauseButtonFuctions.cs
+++ Robo Repo/Assets/Scripts/PauseButtonFuctions.cs
@@ -6,9 +6,16 @@ public class PauseButtonFuctions : MonoBehaviour
 {
     public GameObject pauseRegion;
     public GameObject healthRegion;
+    public GameObject noticeText;
     private void Start()
     {
+        noticeText.SetActive(true);
         ResumeButton();
+        Invoke("Reminder", 4f);
+    }
+    void Reminder()
+    {
+        noticeText.SetActive(false);
     }
     private void Update()
     {
